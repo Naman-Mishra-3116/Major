4f50db89e817b06acfcf9942802b96dca586a7ec
Naman Mishra <125729522+Naman-Mishra-3116@users.noreply.github.com>
2024-08-20 23:48:49 +0530

    .env added tailwind added scrollbar updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,5 @@
-import { useEffect } from "react";
-import axios from "axios";
-import "./App.css";
-
-axios.defaults.withCredentials = true;
 function App() {
-  useEffect(() => {
-    (async () => {
-      try {
-        axios
-          .get("http://localhost:8000/test")
-          .then((resp) => console.log(resp.data))
-          .catch((_)=>console.log("sorry"));
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-  }, []);
-
-  return <>hello</>;
+  return null;
 }
 
 export default App;
