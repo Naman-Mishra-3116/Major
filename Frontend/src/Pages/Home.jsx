af880865ea8b0a7644e8970ce93032619a1f384b
Naman Mishra <125729522+Naman-Mishra-3116@users.noreply.github.com>
2024-08-22 00:55:31 +0530

    Home Page done

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,27 +1,48 @@
-import React from "react";
-import video from "../assets/evermore.mp4";
+import React, { useState } from "react";
+import { theme } from "../Utils/homeVideoArray";
 
 const Home = () => {
+  const [active, setActive] = useState(0);
+  console.log(theme[active].video);
   return (
-    <div className="relative h-lvh">
+    <div className="relative h-screen">
+ 
       <video
-        src={video}
+        src={theme[active].video}
         autoPlay
         loop
         muted
         className="absolute top-0 left-0 h-full w-full object-cover z-0"
       ></video>
-      <div className="relative z-20 flex items-center justify-center h-full pt-[80px]">
-        <div className="text-center text-white">
-          <h1 className="text-4xl font-bold">Welcome to My Website</h1>
-          <p className="text-lg mt-4">
-            Here is some more information about the site.
-          </p>
+
+      <div className="absolute top-0 left-0 h-full w-full bg-black opacity-40 z-10"></div>
+      <div className="absolute top-0 left-0 h-full w-full bg-black opacity-60 z-10"></div>
+
+
+      <div className="absolute z-20 p-20 mt-4 ml-20">
+        <div className="w-full p-5 flex flex-col gap-2 text-white text-left">
+          <h1 className="text-[60px] font-bold">{theme[active].theme}</h1>
+          <div className="w-[50%] mt-4 text-[20px]">{theme[active].text}</div>
         </div>
       </div>
+
+      <footer className="flex justify-center bottom-14 right-[50%] translate-x-[50%] z-20 flex-col items-center absolute">
+        <div className="flex gap-8">
+          {theme.map((_, index) => {
+            return (
+              <div
+                className="w-3 h-3 rounded-full bg-white hover:bg-white cursor-pointer transition-transform duration-300 transform hover:scale-150"
+                key={index}
+                onClick={() => {
+                  setActive(index);
+                }}
+              ></div>
+            );
+          })}
+        </div>
+      </footer>
     </div>
   );
 };
 
 export default Home;
-
---
+++ Frontend/src/Utils/homeVideoArray.js
@@ -0,0 +1,81 @@
+import video1 from "../assets/intro.mp4";
+import video2 from "../assets/fearless.mp4";
+import video3 from "../assets/speak-now.mp4";
+import video4 from "../assets/red.mp4";
+import video5 from "../assets/1989.mp4";
+import video6 from "../assets/reputation.mp4";
+import video7 from "../assets/lover.mp4";
+import video8 from "../assets/folklore.mp4";
+import video9 from "../assets/evermore.mp4";
+import video10 from "../assets/midnights.mp4"
+
+export const theme = [
+  {
+    key: 1,
+    video: video1,
+    theme: "TAYLOR SWIFT",
+    text: `Taylor Swift, the iconic singer-songwriter, has mesmerized listeners with her heartfelt lyrics, captivating melodies, and unwavering authenticity. Having sold over 200 million records worldwide, Taylor Swift is one of the best-selling musicians of all time, released 9 original studio albums, 2 re-recorded studio albums, 5 extended plays (EP), 3 live albums, and 14 miscellaneous compilations. Swift’s career began with a record deal with Big Machine Records in 2005 and the release of her eponymous debut album the following year.
+From her self-titled debut album to her most recent releases, we embark on a chronological journey through Taylor Swift’s musical catalogue. Join us as we explore the depth, emotion, and timeless melodies that have defined her career, uncovering the remarkable narrative woven within the tapestry of her albums. Discover the power and beauty of Taylor Swift’s discography as we navigate the Taylor Swift album order.`,
+  },
+  {
+    key: 2,
+    video: video2,
+    theme: "FEARLESS",
+    text: `Making the transition from teen sensation to mature artist can be difficult, but an 18-year-old Taylor Swift did it with style on her second album, Fearless. The songs are filled with both angst and delight. Swift sails through the title track, “Hey Stephen,” and “You Belong With Me” with boundless joy, as guitars chime and beats bounce. These moments shine even brighter when paired with thoughtful songs like “Fifteen” and “White Horse.”
+Fearless is the second studio album by Taylor Swift, released in 2008. It is a country pop album that features 13 tracks written or co-written by Swift, who also co-produced the album with Nathan Chapman. The album explores themes of romance, heartache, and aspirations, and is inspired by Swift’s teenage feelings. Fearless was a huge commercial and critical success, becoming the best-selling album of 2009 and the most-awarded album in country music history.`,
+  },
+  {
+    key: 3,
+    video: video3,
+    theme: "SPEAK NOW",
+    text: `Taylor Swift’s maturity and assertiveness shine through on Speak Now. Each song is inspired by a personal experience, ranging from unpredictable relationships (“Mine”) to a meticulously crafted response to a very public humiliation (“Innocent”). Swift demonstrates both strength and vulnerability, revealing an artist undergoing creative and personal transformation.
+Speak Now is the third studio album by Taylor Swift, released in 2010. It is a country pop, pop rock, and power pop album that features 14 tracks written entirely by Swift, who also co-produced the album with Nathan Chapman. The album reflects Swift’s transition from adolescence into adulthood, and consists of confessional songs mostly about love and heartbreak that explore past relationships and confront her critics and adversaries.`,
+  },
+  {
+    key: 4,
+    video: video4,
+    theme: "RED",
+    text: `Taylor Swift captures the essence of her fourth album in a primary color: it represents her taste for vengeance, her hot-blooded romantic streak, and the neon-lit pulse of a dance floor. The title track’s banjo pluck and acoustic ballad “All Too Well” will appeal to country fans, but glossy singles like “We Are Never Ever Getting Back Together” and “I Knew You Were Trouble” seem destined for a broader audience—one as vivid as the title suggests.
+Red is the fourth studio album by Taylor Swift, released in 2012. It is a pop, country, and rock album that features 16 tracks written or co-written by Swift, who also co-produced the album with various collaborators. The album explores the intense and conflicting emotions that Swift felt during a turbulent romantic relationship, and is inspired by the color red as a symbol of passion, danger, and heartache.`,
+  },
+  {
+    key: 5,
+    video: video5,
+    theme: "1989",
+    text: `Taylor Swift’s fifth studio album, inspired by the ’80s, is her first “official pop album,” with heavyweights like Max Martin, Shellback, Ryan Tedder, and Jack Antonoff contributing to a sleeker, glitzier sound. “Shake It Off” is reminiscent of OutKast’s own populist anthem “Hey Ya,” and there are echoes of Madonna, Cyndi Lauper, and Belinda Carlisle throughout. nen is a juggernaut, as brash and brilliant as Times Square’s lights.
+1989 is the fifth studio album by Taylor Swift, released in 2014. It is a synth-pop album that features 13 tracks written or co-written by Swift, who also co-produced the album with Max Martin and others. The album marks Swift’s transition from country to pop music, and is inspired by the music and culture of the 1980s. The album explores themes of love, independence, and self-discovery, and is influenced by Swift’s personal experiences and relationships. 1989 was a huge commercial and critical success.`,
+  },
+  {
+    key: 6,
+    video: video6,
+    theme: "REPUTATION",
+    text: `Reputation.
+You don’t have to hear Taylor Swift declare her old self dead—as she does on the explosive “Look What You Made Me Do”—to understand that her new reputation is both a warning shot to her detractors and a full-fledged artistic transformation. All of these songs have a newfound complexity: They’re dark and meaningful, catchy and lived-in, pointed and provocative, and they’re catchy and lived-in. On “End Game,” a languid hip-hop cut with Ed Sheeran and Future, she’s braggadocious, but then sassy and sensual on “…Ready for It?” and “I Did Something Bad.” However, songs like “Call It What You Want” and “Delicate” bring together Taylor’s many emotional layers and confront the dynamic between her celebrity and personal life: “My reputation’s never been worse/So, you must like me for me,” she says. It all adds up to a boundlessly energetic, soul-baring pop masterpiece—her most daring statement yet.`,
+  },
+  {
+    key: 7,
+    video: video7,
+    theme: "LOVER",
+    text: `There’s a reason Taylor Swift sounds so assured and cool on Lover, her seventh and most free-spirited album to date. She’s in love—true, unwavering, starry-eyed, shout-it-from-the-rooftops love. This project comes 13 years after her eponymous debut album, and after a string of songs that felt like battle scars from public breakups and celebrity feuds. It comes across as clear-eyed, thick-skinned, and grown-up. It could be a sign that the 29-year-old has entered a new phase of her life: she’s now impressively private (she and her long-term boyfriend are rarely seen together in public), politically charged (this album finds her fighting for queer and women’s rights), and eager to see the big picture.
+As a result, she’s never sounded more powerful or in command. On the Pride anthem “You Need to Calm Down,” she calls out dark-age bigots, on “The Man,” she mocks patriarchy, on “I Forgot That You Existed,” and on “ME!,” a duet with Panic! at the Disco’s Brendon Urie. At a disco.`,
+  },
+  {
+    key: 8,
+    video: video8,
+    theme: "FOLKLORE",
+    text: "It was only 11 months between the release of Lover and its surprise sequel, but it felt like an eternity. Folklore finds the 30-year-old singer-songwriter teaming up with The National’s Aaron Dessner and long-time collaborator Jack Antonoff for a set of ruminative and relatively lo-fi bedroom pop that’s worlds away from its predecessor. “I’m doing good, I’m on some new st,” Swift begins “the 1,” a sly hybrid of plaintive piano and her naturally bouncy delivery. Swift’s considerable energies, however, have been channeled into writing songs here that double as short stories and character studies, from Proustian flashbacks (“cardigan,” which bears shades of Lana Del Rey) to outcast widows (“the last great american dynasty”) and doomed relationships (“exile,” a heavy-hearted duet with Bon Iver’s Justin Vernon). It’s a work with a lot of texture and imagination. “Your braids like a pattern/Love you to the moon and to Saturn,” she sings on “seven,” a song about two friends planning a getaway.",
+  },
+  {
+    key: 9,
+    video: video9,
+    theme: "EVERMORE",
+    text: `Surprise-releasing a career-defining album in the midst of a paralyzing global pandemic is an admirable flex; doing it again barely five months later is a display of confidence and concentration so audacious that you have every right to feel personally chastised. Evermore, like folklore, is a collaboration with Aaron Dessner, Jack Antonoff, and Justin Vernon, taking advantage of cozy home-studio vibes for more bare-bones arrangements and bared-soul lyrics, casually intimate and narratively rich.
+There is a larger guest roster here—HAIM appears on “no body, no crime,” which seems to place Este Haim in the center of a small-town murder mystery, while Dessner’s bandmates in The National appear on “coney island”—but they fit into the mood rather than distract from it.`,
+  },
+  {
+    key: 10,
+    video: video10,
+    theme: "MIDNIGHT",
+    text: `Taylor Swift’s tenth studio album is a concept album that takes place at night and is about thinking about yourself. This is a collection of music written in the middle of the night, a trip through nightmares and sweet dreams. For all of us who have tossed and turned and decided to keep the lanterns lit and keep looking, hoping that maybe, just maybe, when the clock strikes twelve, we’ll meet ourselves. The album was a commercial success everywhere it came out, setting new standards everywhere. The album sold more than 1.5 million copies in its first week in the US. This made it the fastest-selling album of the year and the best-performing album overall. “Anti-Hero” had ten singles in the top ten on the Billboard Hot 100 in a single week. “Lavender Haze,” “Snow on the Beach,” which featured Lana Del Rey, and “Bejewelled” also made it into the top ten. Swift plans to start the Eras Tour in 2023 to promote both Midnights and her album from before, 1989.`,
+  },
+];
